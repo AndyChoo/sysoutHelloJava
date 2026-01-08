b1ff8cff4bdd2ca3898f8e1ddc012c443c9422c2
Andy Choo <adichoo5585@gmail.com>
2026-01-08 18:57:26 +0800

    add PrintSky

---
+++ src/PrintSky.java
@@ -0,0 +1,45 @@
+public class PrintSky {
+    public static void main(String[] args) throws Exception {
+        while(true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("      .-.                      ");
+            System.out.println("     ( (                  |    ");
+            System.out.println("      `-'               - o -  ");
+            System.out.println("                 .-.      |    ");
+            System.out.println("                (   )          ");
+            System.out.println("                 `-'           ");
+            System.out.println("          _|_                  ");
+            System.out.println("           |                   ");
+
+            Thread.sleep(300);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            System.out.println("      .-.                      ");
+            System.out.println("     ( (                 _|_   ");
+            System.out.println("      `-'                 |    ");
+            System.out.println("                 .-.           ");
+            System.out.println("                (   )          ");
+            System.out.println("           |     `-'           ");
+            System.out.println("         - o -                 ");
+            System.out.println("           |                   ");
+
+            Thread.sleep(300);
+        }
+    }
+}
